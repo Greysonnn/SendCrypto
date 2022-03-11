2945ce329eaaebcff0260c70ed5e708847ab279d
Greysonnn <25331809+Greysonnn@users.noreply.github.com>
2022-03-10 18:55:47 -0700

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/1fwzMDkjOZWYi1uKtIFM1R77PYUumcBY',
+      url: 'https://eth-ropsten.alchemyapi......',
       accounts: [ 'privatekeyredacted' ]
     }
   }
